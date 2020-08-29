cc93ecbf941266d1d23cb1c4ae4c2af98b2052bd
zkonge <zkonge@outlook.com>
2020-08-30 03:05:06 +0800

    Be compatible with the latest IDS

--- ohmyxdu/auth/ids.py
+++ ohmyxdu/auth/ids.py
@@ -1,7 +1,28 @@
+from secrets import token_bytes
+
 from loguru import logger
 from parsel import Selector
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
 
 from ohmyxdu.auth import Auth
+
+__all__ = ('IDSAuth',)
+
+
+def encrypt(key: bytes, value: bytes):
+    """
+    登录时用于加密密码
+
+    :param key:
+    :param value:
+    :return:
+    """
+    # 别学这个登录流程，正确方法可以去看一下非对称密码。
+    # 如果需在不可信信道传递信息的话请优先考虑 TLS1.3+(HTTPS), Noise Protocol 等专业设施
+    # 其实这里只要有一处随机即可，两次随机并不能有效增加安全性
+    box = AES.new(key=key, mode=AES.MODE_CBC, iv=token_bytes(16))
+    return box.encrypt(pad(token_bytes(64) + value, block_size=16))
 
 
 class IDSAuth(Auth):
@@ -32,7 +53,8 @@ class IDSAuth(Auth):
 
         logger.debug(data)
 
-        data['password'] = str(data['password'])
+        key = html.css('input#pwdDefaultEncryptSalt').attrib['value']
+        data['password'] = encrypt(str(data['password']).encode(), key.encode())
 
         self.post(self.AUTH_URL, params=params, data=data)
         self.get(service_url)
